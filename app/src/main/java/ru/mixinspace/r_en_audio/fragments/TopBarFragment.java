35372179d67ca7149824a1fadca992c800379732
MixInSpace <mi.maruas@yandex.ru>
2024-03-26 16:12:07 +0800

    26.03 Added about programm filler

--- app/src/main/java/ru/mixinspace/r_en_audio/fragments/TopBarFragment.java
+++ app/src/main/java/ru/mixinspace/r_en_audio/fragments/TopBarFragment.java
@@ -1,13 +1,23 @@
 package ru.mixinspace.r_en_audio.fragments;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import ru.mixinspace.r_en_audio.R;
@@ -22,14 +32,24 @@ public class TopBarFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        LinearLayout back_button = view.findViewById(R.id.back_button);
+        LinearLayout backButton = view.findViewById(R.id.back_button);
+        LinearLayout menuButton = view.findViewById(R.id.menu_button);
 
-        back_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().finish();
-            }
+        backButton.setOnClickListener(v -> getActivity().finish());
+
+        menuButton.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("Создано для Итогого проекта. \nМарьясов Михаил \nМАОУ \"Гимназия №5\" им. Л.В.Усыниной \n2024 г.");
+            builder.setTitle("О приложении");
+            builder.setCancelable(true);
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
         });
     }
-
 }
